14e5f4d256374c49ea2422dd6b26ce2acfb5516c
RuiOkazaki <okazaki200226@gmail.com>
2022-05-10 22:32:36 +0900

    refactor: change layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -74,16 +74,5 @@ export const Layout: VFC<Props> = ({
     <main className="flex flex-1 flex-col justify-center items-center w-screen">
       {children}
     </main>
-    <footer className="w-full h-12 flex justify-center items-center border-t">
-      <a
-        className="flex items-center"
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </a>
-    </footer>
   </div>
 )
