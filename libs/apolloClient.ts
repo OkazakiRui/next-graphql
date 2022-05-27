7f69bab057cd45b822d02b560c1dbe2a8b655bea
RuiOkazaki <okazaki200226@gmail.com>
2022-05-27 17:31:59 +0900

    feat: x-hasura-admin-secret を追加した

--- libs/apolloClient.ts
+++ libs/apolloClient.ts
@@ -8,8 +8,6 @@ import {
 } from '@apollo/client'
 import 'cross-fetch/polyfill'
 
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
-
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 const createApolloClient = () => {
   return new ApolloClient({
@@ -17,6 +15,9 @@ const createApolloClient = () => {
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
       uri: process.env.NEXT_PUBLIC_HASURA_URL,
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET,
+      },
     }),
     cache: new InMemoryCache(),
   })
